6eada502da8796427c37895fb809765a6f11cec3
NatuMyers <me@natumyers.com>
2017-01-21 17:54:15 -0500

    design overhaul

--- components/NewTodo.jsx
+++ components/NewTodo.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import {Card, Col} from 'react-materialize';
 
 const NewTodo = ({onChange}) => (
-  <div>
-    <h3>New</h3>
-    <input type="text" onKeyUp={onChange}/>
-  </div>
+<Col m={6} s={12}>
+  <Card className='white' textClassName='black-text'>
+      <h3>Add A Lead</h3>
+      <input type="text" onKeyUp={onChange}/>
+  </Card>
+</Col>
 )
 
 export default NewTodo
